ffa2a1fc49ebc6668fa4b378c3e84ceee0d56e25
micha <m.remmos@googlemail.com>
2018-06-30 09:49:31 +0200

    update migrations

---
+++ src/Migrations/Version20180630073637.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180630073637 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE contestants (id INT AUTO_INCREMENT NOT NULL, registration_id INT DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, year VARCHAR(4) NOT NULL, weight_category ENUM(\'-40\',\'-44\',\'-48\',\'-52\',\'-57\',\'-63\',\'-70\',\'+70\',\'-78\',\'+78\'), age_category ENUM(\'cadet\', \'junior\'), itc TINYINT(1) NOT NULL, friday TINYINT(1) NOT NULL, saturday TINYINT(1) NOT NULL, INDEX IDX_25006B6E833D8F43 (registration_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE officials (id INT AUTO_INCREMENT NOT NULL, registration_id INT DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, role VARCHAR(255) DEFAULT NULL, gender ENUM(\'male\', \'female\'), itc TINYINT(1) NOT NULL, friday TINYINT(1) NOT NULL, saturday TINYINT(1) NOT NULL, INDEX IDX_6527C484833D8F43 (registration_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE registrations (id INT AUTO_INCREMENT NOT NULL, club VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, telephone VARCHAR(255) DEFAULT NULL, password VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE contestants ADD CONSTRAINT FK_25006B6E833D8F43 FOREIGN KEY (registration_id) REFERENCES registrations (id)');
+        $this->addSql('ALTER TABLE officials ADD CONSTRAINT FK_6527C484833D8F43 FOREIGN KEY (registration_id) REFERENCES registrations (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE contestants DROP FOREIGN KEY FK_25006B6E833D8F43');
+        $this->addSql('ALTER TABLE officials DROP FOREIGN KEY FK_6527C484833D8F43');
+        $this->addSql('DROP TABLE contestants');
+        $this->addSql('DROP TABLE officials');
+        $this->addSql('DROP TABLE registrations');
+    }
+}
