eb5425ac5aa47f94e58d5d73d317044addcb4481
m0zi <m.remmos@googlemail.com>
2018-10-11 12:19:02 +0200

    add overview to welcome/homepage

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\OfficialRepository;
 use App\Entity\Registration;
+use App\Entity\Transport;
+use App\Repository\ContestantsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\User;
@@ -14,7 +18,29 @@ class WelcomeController extends AbstractController
      */
     public function welcome(): \Symfony\Component\HttpFoundation\Response
     {
+        $registration = $this->getUser();
+
+        $officials = null;
+        $contestants = null;
+        $arrival = null;
+        $departure = null;
+
+        if ($registration) {
+            $officials = $registration->getOfficials();
+            $contestants = $registration->getContestants();
+            $arrival = $registration->getTransports()->filter(function (Transport $transport) {
+                    return $transport->getIsArrival();
+                })->first();
+            $departure = $registration->getTransports()->filter(function (Transport $transport) {
+                    return !$transport->getIsArrival();
+                })->first();
+        }
+
         return $this->render('welcome/index.html.twig', [
+            'officials' => $officials,
+            'contestants' => $contestants,
+            'arrival' => $arrival,
+            'departure' => $departure,
         ]);
     }
 }
