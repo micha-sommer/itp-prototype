68a0c932de7d54ea1ac751a650d601de67ff9bcb
m0zi <m.remmos@googlemail.com>
2019-06-25 19:38:50 +0200

    updated AbstractMigration import

--- src/Migrations/Version20190625171412.php
+++ src/Migrations/Version20190625171412.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -8,15 +10,19 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180830190850 extends AbstractMigration
+final class Version20190625171412 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+        return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE transport (id INT AUTO_INCREMENT NOT NULL, registration_id INT NOT NULL, date DATETIME NOT NULL, time TIME NOT NULL, place VARCHAR(255) NOT NULL, information VARCHAR(255) DEFAULT NULL, INDEX IDX_66AB212E833D8F43 (registration_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE transport ADD CONSTRAINT FK_66AB212E833D8F43 FOREIGN KEY (registration_id) REFERENCES registrations (id)');
+        $this->addSql('ALTER TABLE registrations ADD invoice_address LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -24,6 +30,6 @@ final class Version20180830190850 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE transport');
+        $this->addSql('ALTER TABLE registrations DROP invoice_address');
     }
 }
