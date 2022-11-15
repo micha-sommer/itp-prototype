4bcabf44ad230d56d7a69bfabe65fb0e6414dfed
Michael Sommer <michael.sommer@rewe-digital.com>
2022-11-15 12:39:02 +0100

    add welcome page with internationalization

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('welcome', ['_locale' => 'de']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/welcome', name: 'welcome')]
+    public function index(): Response
+    {
+        return $this->render('welcome/waiting.html.twig');
+    }
+}
