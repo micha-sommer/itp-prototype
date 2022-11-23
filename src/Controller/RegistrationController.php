945ffccd2f02df15c629427eb1a4635e3a07c364
Michael Sommer <michael.sommer@rewe-digital.com>
2022-11-23 16:29:31 +0100

    redirect to `login` after registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -68,7 +68,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('welcome');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('registration/new.html.twig', [
@@ -143,6 +143,6 @@ class RegistrationController extends AbstractController
 
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('welcome');
+        return $this->redirectToRoute('login');
     }
 }
