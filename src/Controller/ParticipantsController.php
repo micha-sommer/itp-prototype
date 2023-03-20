af456534a30c0c082553b5454bb82c78512de417
Michael Sommer <michael.sommer@rewe-digital.com>
2023-03-20 18:14:54 +0100

    add contestant overview by category

--- src/Controller/ParticipantsController.php
+++ src/Controller/ParticipantsController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Registration;
 use App\Form\ContestantsListType;
 use App\Form\OfficialsListType;
+use App\Repository\ContestantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -56,6 +58,30 @@ class ParticipantsController extends AbstractController
 
         return $this->render('participants/officials.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/contestants/age/{ageCategory}/weight/{weightCategory}', name: 'category_overview')]
+    public function contestantsByCategory(
+        string               $ageCategory,
+        string               $weightCategory,
+        ContestantRepository $contestantRepository,
+    ): Response
+    {
+        if ($ageCategory !== 'cadet' && $ageCategory !== 'junior') {
+            throw new BadRequestException("Unsupported age category.");
+        }
+
+        if (!in_array($weightCategory, ['-40', '-44', '-48', '-52', '-57', '-63', '-70', '+70', '-78', '+78'], true)) {
+            throw new BadRequestException("Unsupported weight category.");
+        }
+
+        $contestantInCategory = $contestantRepository->findBy(['ageCategory' => $ageCategory, 'weightCategory' => $weightCategory]);
+
+        return $this->render('participants/category_overview.html.twig', [
+            'ageCategory' => $ageCategory,
+            'weightCategory' => $weightCategory,
+            'contestants' => $contestantInCategory,
         ]);
     }
 }
