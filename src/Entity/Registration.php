c655ac2875846b74420be57c32d049aee86c787c
Michael Sommer <michael.sommer@rewe-digital.com>
2023-12-20 15:32:03 +0100

    update to symfony 6.3

--- src/Entity/Registration.php
+++ src/Entity/Registration.php
@@ -152,7 +152,7 @@ class Registration implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
